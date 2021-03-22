d4362906fd2a6c0535d8cbc9095c816a74d070c8
fatemeh-lohrasbi <lohrasbi.developer@gmail.com>
2021-03-22 16:59:55 +0430

    animation on scroll

--- resource/js/script.js
+++ resource/js/script.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
     $('.js--scroll-to-start-btn').click(function(){
         $('html,body').animate({scrollTop:$('.js--section-features').offset().top},1000);       
     });
-     /* Navigation scroll */    
+     // navigation scroll   
        $("a").on('click', function(event) {     
         if (this.hash !== "") {
           event.preventDefault();
@@ -28,4 +28,25 @@ $(document).ready(function() {
           });
         } 
       });
+      //animation on scroll
+      $('.js--wp-1').waypoint(function(direction) {
+          $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+      }, {
+          offset:'65%'
+      });
+      $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animate__animated animate__fadeInLeftBig');
+    }, {
+        offset:'50%'
+    });
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset:'50%'
+    });
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animate__animated animate__pulse');
+    }, {
+        offset:'50%'
+    });
 });
