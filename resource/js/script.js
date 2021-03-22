d6403b9568a8340a4320c0ecc1014b484216d84d
fatemeh-lohrasbi <lohrasbi.developer@gmail.com>
2021-03-22 15:43:57 +0430

    smooth scrolling

--- resource/js/script.js
+++ resource/js/script.js
@@ -1,5 +1,5 @@
-// sticky nav
 $(document).ready(function() {
+    // sticky nav
     $('.js--section-features').waypoint(function(direction){
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -9,5 +9,23 @@ $(document).ready(function() {
         }, {
         offset:'60px;'
     });  
+    //scroll on buttons
+    $('.js--scroll-to-plan-btn').click(function(){
+        $('html,body').animate({scrollTop:$('.js--section-plans').offset().top},2000);       
+    });
+    $('.js--scroll-to-start-btn').click(function(){
+        $('html,body').animate({scrollTop:$('.js--section-features').offset().top},1000);       
+    });
+     /* Navigation scroll */    
+       $("a").on('click', function(event) {     
+        if (this.hash !== "") {
+          event.preventDefault();
+          var hash = this.hash;
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 1000, function(){
+            window.location.hash = hash;
+          });
+        } 
+      });
 });
-
