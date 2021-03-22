a40da159a04695240a69f7a7a08ec8213072dfc3
fatemeh-lohrasbi <lohrasbi.developer@gmail.com>
2021-03-22 14:13:29 +0430

    sticky navigation

---
+++ resource/js/script.js
@@ -0,0 +1,13 @@
+// sticky nav
+$(document).ready(function() {
+    $('.js--section-features').waypoint(function(direction){
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+           $('nav').removeClass('sticky');
+        }
+        }, {
+        offset:'60px;'
+    });  
+});
+
