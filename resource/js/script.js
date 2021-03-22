69e61204ac05d7ad7a390bc7f4877e60135a1aa1
fatemeh-lohrasbi <lohrasbi.developer@gmail.com>
2021-03-22 19:07:33 +0430

    mobile menu

--- resource/js/script.js
+++ resource/js/script.js
@@ -49,4 +49,17 @@ $(document).ready(function() {
     }, {
         offset:'50%'
     });
+    //mobile navigation
+    $('.js--nav-icon').click(function() {
+        let nav = $('.js--main-nav');
+        let icon = $('.js--nav-icon i');
+        nav.slideToggle(200);
+        if (icon.hasClass('fa-bars')) {
+            icon.addClass('fa-times');
+            icon.removeClass('fa-bars');
+        } else {
+            icon.addClass('fa-bars');
+            icon.removeClass('fa-times');
+        }
+    })
 });
